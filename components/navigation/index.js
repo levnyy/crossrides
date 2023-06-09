33c204fe26b91daeb8ce4a896894d357ef41dedf
Jeremias Zuber <jeremias.zuber@ict-campus.net>
2023-06-09 13:41:16 +0200

    Header with Dropdown Menu

--- components/navigation/index.js
+++ components/navigation/index.js
@@ -4,22 +4,14 @@ export default function Navigation(){
     return(
         <div className={styles.nav}>
                 <nav className={styles.nav}>
-                    <div className={styles.ul}>
-                         <Link href="/Items/Legends" className={styles.nav}>Legends</Link>
-                         <Link href="/Items/Weapons" className={styles.nav}>Weapons</Link>
-                         <Link href="/Items/Maps" className={styles.nav}>Maps</Link>
-                         <Link href="/" className={styles.nav}>Home</Link>
-                            <Link href="/Login" className={styles.nav}>Login</Link>
-                        <Link href="/AboutUs" className={styles.nav}>About Us</Link>
-                        <div className={styles.li}>
-                            <Link href="/" className={styles.w}>Create</Link>
-                            <ul className={styles.ul}>
-                                <li><Link href="/Items/Legends/Create" className={styles.nav}>Legend</Link></li>
-                                <li><Link href="/Items/Maps/Create" className={styles.nav}>Map</Link></li>
-                                <li><Link href="/Items/Weapons/Create" className={styles.nav}>Weapon</Link></li>
-                            </ul>
+                    <div className={styles.dropdown}>
+                        <button className={styles.dropbutton}>Menu</button>
+                        <div className={styles.list}>
+                            <a href="/" className={styles.nav}>Home </a>
+                            <a href="/" className={styles.nav}>Test1 </a>
+                            <a href="/" className={styles.nav}>Test2 </a>
                         </div>
-                    </div>
+                    </div>        
                 </nav>
         </div>
     )
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,10 @@
 import Layout from "@/components/layout";
-import GlobalContextProvider from "@/store";
-import styles from "@/styles/globals.css";
+import styles from "@/styles/global.css";
 
 export default function App({Component, pageProps}) {
     return (
-        <GlobalContextProvider>
             <Layout>
                 <Component {...pageProps} />
             </Layout>
-        </GlobalContextProvider>
     )
 }
