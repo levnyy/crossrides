7a485a1051d0e13b1448b01344aec8e4f7439083
Jeremias Zuber <jeremias.zuber@ict-campus.net>
2023-06-14 09:00:16 +0200

    Home finished with titels and links

--- components/home/index.js
+++ components/home/index.js
@@ -4,11 +4,13 @@ export default function Home() {
     return (
         <div className={styles.body}>
             <div className={styles.schifffahrten}>
+                <h1 className={styles.titel1}>Schifffahrten</h1>
                 <a href='/buchungen'>
                     <img src="ship.png" className={styles.imageship}/>
                 </a>
             </div>
             <div className={styles.buchen}>
+                <h1 className={styles.titel2}>Service</h1>
                 <a href="/service">
                     <img src="services.png" className={styles.imageservice}/>
                 </a>
